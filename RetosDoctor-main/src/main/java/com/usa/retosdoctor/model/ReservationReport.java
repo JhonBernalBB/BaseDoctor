2f2c73448014ab57d3662dbeed38a0892fd81ff3
Jhon Fredi Bernal Burgos <116879832+JhonBernalBB@users.noreply.github.com>
2022-11-21 17:06:52 +0100

    Proyecto Doctor

---
+++ RetosDoctor-main/src/main/java/com/usa/retosdoctor/model/ReservationReport.java
@@ -0,0 +1,16 @@
+package com.usa.retosdoctor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReservationReport {
+    private int completed;
+    private int cancelled;
+
+}
